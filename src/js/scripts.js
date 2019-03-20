d8ed74aef8bdcf51ae9e01d67320aebf588290fb
Beatrice Gorelli <beatrice.gorelli@gmail.com>
2019-03-20 19:03:57 +0100

    Aggiunta Immagini

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,45 @@
-
+// BACKGROUND
 function randomBG (){
 	var r = Math.floor(Math.random()*255);
 	var g = Math.floor(Math.random()*255);
 	var b = Math.floor(Math.random()*255);
+
+	var rgb = "rgb(" + r + ", " + g + "," + b + ")";
+	document.body.style.backgroundColor = rgb;
+}
+
+// HEADER
+function header(){
+	var dati = ["Beatrice Gorelli", "beatrice.gorelli@gmail.com", "SWITZERLAND", "GRAPHIC DESIGNER", "2019"];
+	var selezione = Math.floor(Math.random()*dati.length);
+	document.querySelector(".dati").textContent = dati[selezione].toUpperCase();
+}
+
+// IMMAGINI
+function loadImmagine(){
+	var numImmagini = 3;
+	var xs = document.querySelectorAll(".immagine");
+	for(var i = 0; i < 10; i++){
+		var indice = Math.floor(Math.random() * numImmagini) + 1;	
+		xs[i].src = "src/img/img0" + indice +  ".jpg";
+	}
+	console.log(indice);
+}
+
+// CHIAMARE LE FUNZIONI 
+randomBG();
+
+window.setInterval(function(){
+  loadImmagine();
+  header();
+}, 200);
+
+
+
+
+
+
+
 	/*
 	if(Math.random() < 0.5){
 		r = 255;
@@ -14,30 +51,16 @@ function randomBG (){
 		b = 0;
 	}
 	*/
-	var rgb = "rgb(" + r + ", " + g + "," + b + ")";
-	document.body.style.backgroundColor = rgb;
-}
-
-randomBG();
-
-function header(){
-
-	//var dati = ["Beatrice Gorelli", "beatrice.gorelli@gmail.com", "mi piace il rosso"];
-	var dati = ["Stiamo lavorando per voi !!!!! :) "];
-	var selezione = Math.floor(Math.random()*dati.length);
 	
-	document.querySelector(".dati").textContent = dati[selezione];
-	document.querySelector(".dati1").textContent = dati[selezione];
-	document.querySelector(".dati2").textContent = dati[selezione];
-	document.querySelector(".dati3").textContent = dati[selezione];
-	document.querySelector(".dati4").textContent = dati[selezione];
+	//indice = indice + 1;
 
-}
+	/* IF ELSE 
+	// BOOLEANA: TRUE  / FALSE 
+	if ( indice >= 3 ) { 		
+		indice = 1;
+	} else {
+		indice++;
+	}
+	*/
 
-window.setInterval(function(){
-  header();
-}, 200);
-
-window.setInterval(function(){
-  randomBG();
-}, 300);
+	//console.log(indice);

cd365f4e818b6db6adac77f711b893ba3f754f01
Beatrice Gorelli <beatrice.gorelli@gmail.com>
2019-03-20 17:37:20 +0100

    Aggiornato Js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,6 @@ function randomBG (){
 	var r = Math.floor(Math.random()*255);
 	var g = Math.floor(Math.random()*255);
 	var b = Math.floor(Math.random()*255);
-
 	/*
 	if(Math.random() < 0.5){
 		r = 255;
@@ -15,32 +14,30 @@ function randomBG (){
 		b = 0;
 	}
 	*/
-
-
 	var rgb = "rgb(" + r + ", " + g + "," + b + ")";
 	document.body.style.backgroundColor = rgb;
-
-
-
 }
 
 randomBG();
 
 function header(){
 
-	var dati = ["Beatrice Gorelli", "beatrice.gorelli@gmail.com", "079 239 19 19", "mi piace il rosso"];
+	var dati = ["Beatrice Gorelli", "beatrice.gorelli@gmail.com", "mi piace il rosso"];
+	//var dati = ["Stiamo lavorando per voi !!!!! :) "];
 	var selezione = Math.floor(Math.random()*dati.length);
+	
 	document.querySelector(".dati").textContent = dati[selezione];
 	document.querySelector(".dati1").textContent = dati[selezione];
 	document.querySelector(".dati2").textContent = dati[selezione];
 	document.querySelector(".dati3").textContent = dati[selezione];
 	document.querySelector(".dati4").textContent = dati[selezione];
+
 }
 
 window.setInterval(function(){
   header();
-}, 100);
+}, 200);
 
 window.setInterval(function(){
   randomBG();
-}, 400);
+}, 300);
